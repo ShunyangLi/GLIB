10a6ab4f68d3d8fe5b60ee3e04580ab6b39fd734
Shunyang Li <shunyangli0@gmail.com>
2024-12-07 14:22:07 +1100

    add kcore

--- include/glib/cohesive/algorithm.h
+++ include/glib/cohesive/algorithm.h
@@ -7,4 +7,53 @@
 #ifndef GLIB_ALGORITHM_H
 #define GLIB_ALGORITHM_H
 
+#include "glib/graph.h"
+
+namespace GLIB {
+    template<typename VertexType>
+    auto query_kcore(const Graph<VertexType>& graph, uint k) -> std::vector<VertexType> {
+
+        if (k <= 0) {
+            log_error("k should be greater than 0");
+            exit(EXIT_FAILURE);
+        }
+
+        auto is_deleted = std::vector<bool>(graph.n, false);
+        auto degree = std::vector<int>(graph.n, 0);
+        auto invalid = std::vector<int>();
+
+        for (int i = 0; i < graph.n; i++) {
+            degree[i] = graph.adj[i].size();
+            if (degree[i] < k) invalid.push_back(i);
+        }
+
+        while (!invalid.empty()) {
+            auto u = invalid.back();
+            invalid.pop_back();
+            is_deleted[u] = true;
+
+            for (const auto& v : graph.adj[u]) {
+                if (!is_deleted[v]) {
+                    degree[v]--;
+                    if (degree[v] < k) invalid.push_back(v);
+                }
+            }
+        }
+
+        auto kcore = std::vector<VertexType>();
+        for (int i = 0; i < graph.n; i++) {
+            if (!is_deleted[i]) kcore.push_back(graph.vertices[i]);
+        }
+
+        return kcore;
+    }
+
+    template<typename VertexType>
+    auto kcore_decomposition(const Graph<VertexType>& graph) -> std::unordered_map<VertexType, int> {
+
+    }
+
+} // namespace GLIB
+
+
 #endif //GLIB_ALGORITHM_H
